0fac57d68490cb16a13b291413118a57c3c8d6a2
sirus-the-beaver <salaris@oregonstate.edu>
2024-01-21 01:26:14 -0800

    Create JavaScript app for form validation

---
+++ app/static/js/app.js
@@ -0,0 +1,30 @@
+const submit = document.querySelector('input[type="submit"]');
+const mood_radios = document.querySelectorAll('input[type="radio"][name="mood"]');
+const energy_radios = document.querySelectorAll('input[type="radio"][name="energy"]');
+const dance_radios = document.querySelectorAll('input[type="radio"][name="dance"]');
+
+submit.addEventListener('click', function(event) {
+    // Prevent the form from submitting if the user hasn't selected a mood, energy, and danceability
+    mood_selected = false;
+    for (mood_radio of mood_radios) {
+        if (mood_radio.checked) {
+            mood_selected = true;
+        }
+    }
+    energy_selected = false;
+    for (energy_radio of energy_radios) {
+        if (energy_radio.checked) {
+            energy_selected = true;
+        }
+    }
+    dance_selected = false;
+    for (dance_radio of dance_radios) {
+        if (dance_radio.checked) {
+            dance_selected = true;
+        }
+    }
+    if (!mood_selected || !energy_selected || !dance_selected) {
+        event.preventDefault();
+        alert('Please select a mood, energy, and danceability');
+    }
+});
